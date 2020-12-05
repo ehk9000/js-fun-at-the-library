75879590acdeb48eff98aec8d95cf5ca8ae84c23
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2020-12-04 19:52:18 -0600

    pass shelf-test.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,37 @@
+function shelfBook(book, shelf) {
+  if (shelf.length <= 2) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(title, shelf) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      shelf = shelf.splice([i],1)
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var names = []
+  for (i = 0; i < shelf.length; i++) {
+    names.push(shelf[i].title)
+  }
+  return names.join(', ')
+}
+
+function searchShelf(shelf, title) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
